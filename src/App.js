fc74c3c0b744aadabfeba5ac34770bbaca4c618f
artur <me@zalart.com>
2021-03-03 19:24:16 +0300

    Quotecard on Click via axios get created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import QuoteCard from './components/QuoteCard';
+import Axios from 'axios';
+import './App.css'
+const quote = { "quote": "Shut up, brain. I got friends now. I don't need you anymore.", "character": "Lisa Simpson", "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083", "characterDirection": "Right" };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteCard: quote
+    }
+    this.getQuote = this.getQuote.bind(this);
+  }
+  getQuote(event) {
+    event.preventDefault();
+    const url = 'https://simpsons-quotes-api.herokuapp.com/quotes';
+    Axios.get(url)
+      .then(result => result.data)
+      .then(data => {
+        this.setState({
+          quoteCard: data[0]
+        })
+      })
+  }
+
+  render() {
+    const quoteCard = this.state.quoteCard;
+    return (
+      <div className="App" >
+        <button onClick={this.getQuote}>Get quoteCard</button>
+        <QuoteCard {...quoteCard} />
+
+      </div>
+    );
+  }
 }
 
-export default App;
